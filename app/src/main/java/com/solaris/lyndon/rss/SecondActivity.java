c2f125fa1c6ffee226218444df3287215dd60e56
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2014-10-14 00:47:54 -0500

    WebView set for content in ScrollView. Styled and push to Chrome for web Intent created.

--- app/src/main/java/com/solaris/lyndon/rss/SecondActivity.java
+++ app/src/main/java/com/solaris/lyndon/rss/SecondActivity.java
@@ -1,15 +1,29 @@
 package com.solaris.lyndon.rss;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.protocol.HTTP;
 
 
 public class SecondActivity extends Activity {
 
-    protected TextView title, publishDate, content, url;
+    protected TextView title, publishDate, url;
+    protected WebView content;
+    protected final String MIME_TYPE = "text/html";
+    protected final String ENCODING = "UTF-8";
+    protected ListItem intentCurrentItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +32,52 @@ public class SecondActivity extends Activity {
 
         // Fetching data from a parcelable object passed from MainActivity
         ListItem currentItem = getIntent().getParcelableExtra("currentItem");
+        intentCurrentItem = currentItem;
+
+        EventHandler eventHandler = new EventHandler();
 
         title = (TextView)findViewById(R.id.title2);
         publishDate = (TextView)findViewById(R.id.pub_date2);
-        content = (TextView)findViewById(R.id.content);
+        content = (WebView)findViewById(R.id.content);
         url = (TextView)findViewById(R.id.id_url);
 
         title.setText(currentItem.getTitle());
         publishDate.setText(currentItem.getPublishDate());
-        content.setText(currentItem.getContent()); // WebView this shit later as the content has HTML to format
         url.setText(currentItem.getUrl());
 
+        //content.getSettings().setJavaScriptEnabled(true);
+        content.loadDataWithBaseURL("", currentItem.getContent(), MIME_TYPE, ENCODING, "");
+        content.setOnClickListener(eventHandler);
+
+        title.setOnClickListener(eventHandler);
+        url.setOnClickListener(eventHandler);
 
     }
+
+    class EventHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            if (v.getId() == R.id.title2 || v.getId() == R.id.id_url){
+                startWebIntent(intentCurrentItem.getUrl());
+            }
+        }
+    }
+
+    //Programmed to specifically open in chrome
+    public void startWebIntent(String url){
+        try {
+            Intent intent = new Intent();
+            intent.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+            intent.addCategory("android.intent.category.LAUNCHER");
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "You don't have Chrome? WTF?", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 
     @Override
